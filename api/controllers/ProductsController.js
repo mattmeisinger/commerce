12e53e157b55df53f69fc93d6d690b03025fb031
Matthew Meisinger <mmeisinger@gmail.com>
2014-11-22 17:09:18 -0500

    Added controller stubs

---
+++ api/controllers/ProductsController.js
@@ -0,0 +1,11 @@
+/**
+ * ProductsController
+ *
+ * @description :: Server-side logic for managing products
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
