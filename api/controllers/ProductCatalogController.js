649826a1f35dc0c423d3c5baf5ffde2b3fb80f9e
Johan Mena <johan.menac@gmail.com>
2014-12-10 22:37:27 -0500

    Set up dynamodb backend for ProductCatalog

---
+++ api/controllers/ProductCatalogController.js
@@ -0,0 +1,11 @@
+/**
+ * ProductCatalogController
+ *
+ * @description :: Server-side logic for managing productcatalogs
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
--- config/connections.js
+++ config/connections.js
@@ -32,6 +32,10 @@ module.exports.connections = {
     adapter: 'sails-disk'
   },
 
+  dynamoDb: {
+    adapter: "sails-dynamodb"
+  },
+
   /***************************************************************************
   *                                                                          *
   * MySQL is the world's most popular relational database.                   *
