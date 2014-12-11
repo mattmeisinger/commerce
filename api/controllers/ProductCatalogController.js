84c783fac27ba65be38bbc690128c105e7dd6f72
Johan Mena <johan.menac@gmail.com>
2014-12-10 22:44:27 -0500

    Generate ProductController placeholders.

--- api/controllers/ProductCatalogController.js
+++ api/controllers/ProductCatalogController.js
@@ -6,6 +6,55 @@
  */
 
 module.exports = {
-	
+
+  /**
+   * `ProductCatalogController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `ProductCatalogController.destroy()`
+   */
+  destroy: function (req, res) {
+    return res.json({
+      todo: 'destroy() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `ProductCatalogController.findByProductId()`
+   */
+  findByProductId: function (req, res) {
+    return res.json({
+      todo: 'findByProductId() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `ProductCatalogController.findByCategory()`
+   */
+  findByCategory: function (req, res) {
+    return res.json({
+      todo: 'findByCategory() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `ProductCatalogController.findByRating()`
+   */
+  findByRating: function (req, res) {
+    return res.json({
+      todo: 'findByRating() is not implemented yet!'
+    });
+  }
+
 };
 
